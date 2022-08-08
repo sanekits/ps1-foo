5acef73201704f5e74820e86416e8dcaf5698943
Les Matheson <les.matheson@gmail.com>
2022-08-08 19:37:23 -0400

    Sync (auto)

--- bin/setup.sh
+++ bin/setup.sh
@@ -16,26 +16,26 @@ reload_reqd=false
 
 base_defs=${Scriptdir}/../shellkit/shellkit_setup_base
 [[ -f ${base_defs} ]] || die "Can't find ${base_defs}"
-source ${base_defs} || die Failed sourcing shellkit_base
+source "${base_defs}" || die Failed sourcing shellkit_base
 
 main() {
-    if [[ ! -d $HOME/.local/bin/ps1-foo ]]; then
-        if [[ -e $HOME/.local/bin/ps1-foo ]]; then
-            die "$HOME/.local/bin/ps1-foo exists but is not a directory.  Refusing to overwrite"
+    if [[ ! -d $HOME/.local/bin/${Kitname} ]]; then
+        if [[ -e $HOME/.local/bin/${Kitname} ]]; then
+            die "$HOME/.local/bin/${Kitname} exists but is not a directory.  Refusing to overwrite"
         fi
-        command mkdir -p $HOME/.local/bin/ps1-foo || die "Failed creating $HOME/.local/bin/ps1-foo"
+        command mkdir -p $HOME/.local/bin/${Kitname} || die "Failed creating $HOME/.local/bin/${Kitname}"
     fi
-    if [[ $(inode $Script) -eq $(inode ${HOME}/.local/bin/ps1-foo/setup.sh) ]]; then
+    if [[ $(inode $Script) -eq $(inode ${HOME}/.local/bin/${Kitname}/setup.sh) ]]; then
         die "cannot run setup.sh from ${HOME}/.local/bin"
     fi
-    builtin cd ${HOME}/.local/bin/ps1-foo || die "101"
+    builtin cd ${HOME}/.local/bin/${Kitname} || die "101"
     command rm -rf ./* || die "102"
     [[ -d ${Scriptdir} ]] || die "bad Scriptdir [$Scriptdir]"
     command cp -r ${Scriptdir}/* ./ || die "failed copying from ${Scriptdir} to $PWD"
     builtin cd .. # Now were in .local/bin
-    command ln -sf ./ps1-foo/ps1-foo-version.sh ./ || die "101.5"
-    command ln -sf ./ps1-foo/parse_ps1_host_suffix.sh ./ || die "101.6"
-    path_fixup_local_bin ps1-foo || die "102"
+    command ln -sf ./${Kitname}/${Kitname}-version.sh ./ || die "101.5"
+    command ln -sf ./${Kitname}/parse_ps1_host_suffix.sh ./ || die "101.6"
+    path_fixup_local_bin ${Kitname} || die "102"
     shrc_fixup || die "104"
     $reload_reqd && builtin echo "Shell reload required ('bash -l')" >&2
 }
