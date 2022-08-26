5da2988421442a11c2b38727ca36c8d02c7541cc
Les Matheson <les.matheson@gmail.com>
2022-08-26 17:49:06 -0400

    Sync (auto)

--- bin/ps1-foo-version.sh
+++ bin/ps1-foo-version.sh
@@ -2,7 +2,7 @@
 
 # Running ps1-foo-version.sh is the correct way to
 # get the home path for ps1-foo and its tools.
-Ps1FooVersion=0.7.5
+Ps1FooVersion=0.7.6
 
 set -e
 
@@ -11,5 +11,5 @@ scriptDir=$(dirname -- "$Script")
 
 
 if [ -z "$sourceMe" ]; then
-    printf "%s\t%s" ${scriptDir}/ps1-foo ${Ps1FooVersion}
+    printf "%s\t%s" ${scriptDir} ${Ps1FooVersion}
 fi
