cdd72bd5fa7cb66d18fc4aae6385a88907492967
Les Matheson <lmatheson4@bloomberg.net>
2022-10-09 06:32:06 -0400

    switch to prompt-command-wrap v2

--- make-kit.mk
+++ make-kit.mk
@@ -11,6 +11,8 @@ kit_depends := \
     bin/ps1-foo.bashrc \
     bin/ps1-foo.sh
 
+pcw_depends := $(shell $(MAKE) -s -C ../prompt-command-wrap pcw-deps)
+
 .PHONY: publish
 
 
@@ -22,12 +24,11 @@ publish: pre-publish publish-common release-draft-upload release-list
 	@echo ">>>> and then change ./version to avoid accidental confusion. <<<<"
 	cat tmp/draft-url
 
-.PHONY: prompt-command-wrap
-prompt-command-wrap:
+bin/prompt-command-wrap.bashrc:  $(pcw_depends)
 	$(MAKE) -C ../prompt-command-wrap build
 	cp ../prompt-command-wrap/tmp/prompt-command-wrap.bashrc bin/
 
-build: prompt-command-wrap
+build: bin/prompt-command-wrap.bashrc
 
 
 build:
