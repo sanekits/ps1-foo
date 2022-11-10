da1d1f481f2e5cb9d413ab80353bb9d46f52b423
Les Matheson <lmatheson4@bloomberg.net>
2022-11-09 20:01:24 -0500

    Sync (auto)

--- bin/ps1-foo-version.sh
+++ bin/ps1-foo-version.sh
@@ -2,7 +2,7 @@
 
 # Running ps1-foo-version.sh is the correct way to
 # get the home install path for the tool
-KitVersion=0.8.7
+KitVersion=0.8.8
 
 canonpath() {
     builtin type -t realpath.sh &>/dev/null && {
