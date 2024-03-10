87456320b909f36da9cdc8e62208d4fc9bbbb7b6
LES MATHESON <LMATHESON4@bloomberg.net>
2024-03-10 11:32:20 +0000

    Upgrade shellkit loader

--- bin/ps1-foo-version.sh
+++ bin/ps1-foo-version.sh
@@ -2,7 +2,7 @@
 
 # Running ps1-foo-version.sh is the correct way to
 # get the home install path for the tool
-KitVersion=1.0.1
+KitVersion=1.0.2
 
 canonpath() {
     builtin type -t realpath.sh &>/dev/null && {
