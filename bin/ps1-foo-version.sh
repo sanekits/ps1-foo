118e56d9ee04c8a07c22361dd732db88fb02b918
LES MATHESON <LMATHESON4@bloomberg.net>
2025-04-12 13:43:02 +0000

    Reconcile with template v2

--- bin/ps1-foo-version.sh
+++ bin/ps1-foo-version.sh
@@ -4,6 +4,14 @@
 # get the home install path for the tool
 KitVersion=1.0.3
 
+# The shellkit/ tooling naturally evolves out from under the dependent kits.  ShellkitSetupVers allows
+# detecting the need for refresh of templates/* derived files.  To bump the root version, 
+# zap all templates/* containing 'ShellkitTemplateVers' constants and changes to the corresponding dependent kits
+# Note that within templates/* there may be diverse versions in upstream shellkit, they don't all have to match,
+# but the derived copies should be sync'ed with upstream as needed.
+#shellcheck disable=2034
+ShellkitTemplateVers=2
+
 canonpath() {
     builtin type -t realpath.sh &>/dev/null && {
         realpath.sh -f "$@"
@@ -14,7 +22,7 @@ canonpath() {
         return
     }
     # Fallback: Ok for rough work only, does not handle some corner cases:
-    ( builtin cd -L -- "$(command dirname -- $0)"; builtin echo "$(command pwd -P)/$(command basename -- $0)" )
+    ( builtin cd -L -- "$(command dirname -- "$0")" || exit; builtin echo "$(command pwd -P)/$(command basename -- "$0")" )
 }
 
 Script=$(canonpath "$0")
@@ -22,5 +30,5 @@ Scriptdir=$(dirname -- "$Script")
 
 
 if [[ -z "$sourceMe" ]]; then
-    builtin printf "%s\t%s\n" ${Scriptdir} $KitVersion
+    builtin printf "%s\t%s\n" "${Scriptdir}" $KitVersion
 fi
--- make-kit.mk
+++ make-kit.mk
@@ -3,6 +3,13 @@
 #  It defines values that are kit-specific.
 #  You should edit it and keep it source-controlled.
 
+# The shellkit/ tooling naturally evolves out from under the dependent kits.  ShellkitSetupVers allows
+# detecting the need for refresh of templates/* derived files.  To bump the root version, 
+# zap all templates/* containing 'ShellkitTemplateVers' constants and changes to the corresponding dependent kits
+# Note that within templates/* there may be diverse versions in upstream shellkit, they don't all have to match,
+# but the derived copies should be sync'ed with upstream as needed.
+ShellkitTemplateVers=2
+
 # TODO: update kit_depends to include anything which
 #   might require the kit version to change as seen
 #   by the user -- i.e. the files that get installed,
@@ -11,6 +18,7 @@ kit_depends := \
     bin/ps1-foo.bashrc \
     bin/ps1-foo.sh
 
+.PHONY: publish publish-draft
 pcw_depends := $(shell $(MAKE) -s -C ../prompt-command-wrap pcw-deps)
 build_depends += $(../prompt-command-wrap/tmp/promp-command-wrap.bashrc)
 
@@ -33,5 +41,6 @@ publish: pre-publish publish-common release-upload release-list
 	cat tmp/draft-url
 	@echo ">>>> publish complete OK. (FINAL)  <<<"
 
-build:
-	@echo Build OK
+publish-draft: pre-publish publish-common release-draft-upload release-list
+	cat tmp/draft-url
+	@echo ">>>> publish complete OK. (DRAFT - you must manually publish it from github release page)  <<<"
